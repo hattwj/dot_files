872c4e53a5b0ef5bcb60f40681f9d94aef52ba5c
William Hatt <hatt@amazon.com>
2022-05-04 14:48:04 -0700

    Update installer

--- installers/ubuntu/build_tools.sh
+++ installers/ubuntu/build_tools.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/env bash
 set -e
 
-sudo apt install make git build-essential ruby terminator libncurses-dev
+sudo apt install \
+  make \
+  git \
+  build-essential \
+  ruby \
+  terminator \
+  libncurses-dev \
+  silversearcher-ag
