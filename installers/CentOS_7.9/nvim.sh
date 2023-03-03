73942f758455d208fc5e156a4f2058039f2fd2b0
William Hatt <hatt@amazon.com>
2023-03-03 21:22:56 +0000

    installers

--- installers/CentOS_7.9/nvim.sh
+++ installers/CentOS_7.9/nvim.sh
@@ -6,5 +6,5 @@ pushd ~/Downloads && \
   wget https://github.com/neovim/neovim/releases/download/nightly/nvim.appimage -O nvim && \
   chmod ugo+x ./nvim && \
   mv ./nvim ~/.bin/nvim && \
-  sudo /usr/bin/yum -y install fuse && \
+  sudo /usr/bin/yum -y install fuse fuse-libs && \
   echo yay || echo boo
