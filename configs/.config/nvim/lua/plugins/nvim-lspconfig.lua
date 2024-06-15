97e44f3ed3b1fd6de6a32731a136a1b51cd5b8e4
William Hatt <hatt@amazon.com>
2024-06-15 07:06:51 +0000

    lsp

---
+++ configs/.config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -0,0 +1,39 @@
+return {
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        bashls = {
+          filetypes = { "sh", "zsh" },
+        },
+        solargraph = {
+          -- https://github.com/castwide/solargraph/issues/483#issuecomment-926516460
+          -- these two options disable rubocop
+          autoformat = false,
+          formatting = false,
+
+          cmd = { "solargraph"}
+        },
+        -- This looks like it disables the lsp,
+        -- but as far as I can tell it just prevents
+        -- installing it, otherwise still used. 
+        -- Allowing manual installation
+        ruff_lsp = false,
+        -- ruff_lsp = {
+        --   cmd = { "ruff-lsp" }
+        -- },
+        -- ruby_lsp = {
+        --   cmd = { "rvm use 3.1.0 && ruby-lsp" },
+        --   init_options = {
+        --     formatter = "auto",
+        --   },
+        -- },
+        -- rubocop = {
+        --   -- See: https://docs.rubocop.org/rubocop/usage/lsp.html
+        --   cmd = { "bundle", "exec", "rubocop", "--lsp" },
+        --   root_dir = lspconfig.util.root_pattern("Gemfile", ".git", "."),
+        -- },
+      },
+    },
+  },
+}
