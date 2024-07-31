36dfd6824ac24ad86c764e42693a8a2c3584ce29
William Hatt <hatt@amazon.com>
2024-07-31 17:09:12 +0000

    Add link to explain why we are building from source

--- installers/AL2/tmux.sh
+++ installers/AL2/tmux.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 set -e
 
+# Fix for tmux crashing when running nvim
+# https://github.com/tmux/tmux/issues/3983
+
 cd ~/src
 [ ! -d ~/src/tmux ] && git clone https://github.com/tmux/tmux.git
 cd tmux
