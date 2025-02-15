9b9c1f7b15437ef29eddddb5a173512c7fa39b51
William Hatt <william.hatt@elementaltechnologies.com>
2025-02-15 15:23:19 -0800

    sshCopySwap - Enable dropdown menu

--- configs/.config/nvim/custom-plugins/ssh_copy_swap.lua
+++ configs/.config/nvim/custom-plugins/ssh_copy_swap.lua
@@ -20,7 +20,6 @@ local function get_remote_hosts()
 
   local hosts = {}
   for host in result:gmatch("[^\r\n]+") do
-    print("sshCopySwap - Found host: " .. host)
     table.insert(hosts, host)
   end
   return hosts
@@ -45,9 +44,13 @@ local function rsync_file(local_path, remote_host, remote_path, direction)
   else
     cmd = string.format("timeout 9 rsync -ave ssh %s:%s %s", remote_host, remote_path, local_path)
   end
-  print('sshCopySwap - Running command: ' .. cmd)
+  vim.api.nvim_notify("sshCopySwap running command: " .. cmd, 1, {})
   -- runs command on a sub-process.
   local handle = io.popen(cmd)
+  if handle ~= nil then
+    error("Rsync operation failed or timed out")
+    return 1
+  end
   -- reads command output.
   local output = handle:read('*a')
   local exit_code = handle:close()
@@ -58,52 +61,7 @@ local function rsync_file(local_path, remote_host, remote_path, direction)
   return exit_code
 end
 
-function M.xcopy()
-  ensure_local_dir()
-  local clipboard = is_ssh() and vim.fn.getreg('"') or vim.fn.getreg('*')
-  local local_path = os.getenv("HOME") .. "/.local/.nvim_xcopy"
-  local remote_path = "~/.local/.nvim_xpaste"
-
-  local file = io.open(local_path,"w+")
-  file:write(clipboard)
-  file:close()
-
-  if not is_ssh() then
-    local hosts = get_remote_hosts()
-    if #hosts == 0 then
-      print("No active SSH connections found. Aborting XCopy.")
-      return
-    end
-    local selected_host = select_host(hosts)
-    if selected_host then
-      local exit_code = pcall(rsync_file, local_path, selected_host, remote_path, "push")
-      if exit_code == 0 then
-        return
-      end
-    end
-  end
-end
-
-function M.xpaste()
-  ensure_local_dir()
-  local local_path = os.getenv("HOME") .. "/.local/.nvim_xpaste"
-  local remote_path = "~/.local/.nvim_xcopy"
-
-  if not is_ssh() then
-    local hosts = get_remote_hosts()
-    if #hosts == 0 then
-      print("No active SSH connections found. Aborting XPaste.")
-      return
-    end
-    local selected_host = select_host(hosts)
-    if selected_host then
-      local exit_code = pcall(rsync_file, local_path, selected_host, remote_path, "pull")
-      if exit_code == 0 then
-        return
-      end
-    end
-  end
-
+local function fileToClipboard(local_path, args)
   local file = assert(io.open(local_path, "rb"))
   local data = file:read("*all")
   file:close()
@@ -115,20 +73,102 @@ function M.xpaste()
     vim.fn.setreg('"', data)
   else
     vim.fn.setreg('*', data)
+    vim.fn.setreg('+', data)
   end
+  if args == 'p' then
+    vim.api.nvim_paste(data, true, -1)
+  end
+end
+
+local function pasteToHost(local_path, remote_path, selected_host, args)
+  local exit_code = pcall(rsync_file, local_path, selected_host, remote_path, "pull")
+  if exit_code == 0 then
+    vim.api.nvim_notify("XPaste failed: rsync error", 4, {})
+    return
+  end
+  fileToClipboard(local_path, args)
+end
+
+local function copyToHost(local_path, remote_path, selected_host)
+  local exit_code = pcall(rsync_file, local_path, selected_host, remote_path, "push")
+  if exit_code == 0 then
+    vim.api.nvim_notify("XCopy failed: rsync error", 4, {})
+    return
+  end
+end
+
+function M.xcopy()
+  ensure_local_dir()
+  local clipboard = is_ssh() and vim.fn.getreg('"') or vim.fn.getreg('+')
+  local local_path = os.getenv("HOME") .. "/.local/.nvim_xcopy"
+  local remote_path = "~/.local/.nvim_xpaste"
+
+  local file = io.open(local_path,"w+")
+  file:write(clipboard)
+  file:close()
+
+  if is_ssh() then
+    -- Nothing left to do
+    return
+  end
+
+  local hosts = get_remote_hosts()
+  if #hosts == 0 then
+    vim.api.nvim_notify("XCopy: No active SSH connections found", 4, {})
+    return
+  end
+
+  if #hosts == 1 then
+    copyToHost(local_path, remote_path, hosts[1])
+    return
+  end
+
+  vim.ui.select(hosts, {prompt="xCopy: select target host"}, function(selected_host)
+    if selected_host then
+      copyToHost(local_path, remote_path, selected_host)
+    end
+  end)
+end
+
+function M.xpaste(opts)
+  ensure_local_dir()
+  local local_path = os.getenv("HOME") .. "/.local/.nvim_xpaste"
+  local remote_path = "~/.local/.nvim_xcopy"
+
+  if is_ssh() then
+    fileToClipboard(local_path, opts.args)
+    return
+  end
+
+  local hosts = get_remote_hosts()
+  if #hosts == 0 then
+    vim.api.nvim_notify("XPaste: No active SSH connections found", 4, {})
+    return
+  end
+
+  if #hosts == 1 then
+    pasteToHost(local_path, remote_path, hosts[1], opts.args)
+    return
+  end
+
+  vim.ui.select(hosts, {prompt="xPaste: select target host"}, function(selected_host)
+    if selected_host then
+      pasteToHost(local_path, remote_path, selected_host, opts.args)
+    end
+  end)
 end
 
 -- Set up commands
 vim.api.nvim_create_user_command('XCopy', M.xcopy, {})
-vim.api.nvim_create_user_command('XPaste', M.xpaste, {})
+vim.api.nvim_create_user_command('XPaste', M.xpaste, {nargs='?'})
 
 -- Set up key mappings for normal and visual mode
 vim.keymap.set({'n'}, '<C-y>', '<cmd>XCopy<CR>', {noremap = true, silent = true})
-vim.keymap.set({'n'}, '<C-p>', '<cmd>XPaste<CR>p', {noremap = true, silent = true})
+vim.keymap.set({'n'}, '<C-p>', '<cmd>XPaste p<CR>', {noremap = true, silent = true})
 
 --  In visual mode, yank the current selection, and then copy it to the remote machine
 vim.keymap.set({'v'}, '<C-y>', 'y<cmd>XCopy<CR>', {noremap = true, silent = true})
--- In visual mode, delete the current selection, and then copy the data from the remote machine
-vim.keymap.set({'v'}, '<C-p>', 'x<cmd>XPaste<CR>', {noremap = true, silent = true})
+-- In visual mode, delete the current selection, and then copy the data from the remote machine and paste it
+vim.keymap.set({'v'}, '<C-p>', 'x<cmd>XPaste p<CR>', {noremap = true, silent = true})
 
 return M
