0d57f48e74c168f9831ad9201a2c1902e555f8a3
William Hatt <hatt@amazon.com>
2025-04-09 22:28:27 +0000

    wip

---
+++ configs/.config/nvim/lua/plugins/noice.lua
@@ -0,0 +1,29 @@
+return {
+  "folke/noice.nvim",
+  event = "VeryLazy",
+  opts = {
+    views = {
+      mini = { timeout = 8500, },
+    },
+    cmdline = {
+      view = "cmdline",
+    },
+    routes = {
+      { -- When a message is more than 40 characters wide, show it as a custom message 
+        -- with custom rules.
+        -- view = "virtualtext",
+        view = "mini",
+        filter = { event = "msg_show", min_width = 40 },
+      },
+    },
+  },
+  config = function(_, opts)
+    -- HACK: noice shows messages from before it was enabled,
+    -- but this is not ideal when Lazy is installing plugins,
+    -- so clear the messages in this case.
+    -- if vim.o.filetype == "lazy" then
+    --   vim.cmd([[messages clear]])
+    -- end
+    require("noice").setup(opts)
+  end,
+}
